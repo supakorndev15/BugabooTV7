01cfc91689729088796eddce4fb690132e1281c9
MacDetail <supakorndev15@gmail.com>
2025-07-21 15:18:09 +0700

    svg

--- BugabooTV7/Cell/CatListCollectionViewCell.swift
+++ BugabooTV7/Cell/CatListCollectionViewCell.swift
@@ -1,7 +1,8 @@
 
 import UIKit
 import Kingfisher
-//import KingfisherSVG
+import SDWebImage
+import SDWebImageSVGCoder
 
 class CatListCollectionViewCell: UICollectionViewCell {
 
@@ -13,31 +14,36 @@ class CatListCollectionViewCell: UICollectionViewCell {
     
     func configure(with menu: Menu) {
         titleLabel.text = menu.label
-//        loadImage(from: menu.iconURL)
-        guard let url = URL(string: "https://static.bugaboo.tv/mobile/images/super-app/icon/icon_shopping_bag_01.svg") else { return }
-        iconImageView.kf.setImage(with: url)
-        print("url : \(url)")
+        loadImage(from: menu.iconURL)
     }
     
     private func loadImage(from urlString: String) {
         guard let url = URL(string: urlString) else {
-            iconImageView.image = UIImage(named: "magnifyingglass")
+            iconImageView.image = UIImage(named: "placeholder")
             return
         }
+
+        print("โหลดรูปจาก: \(url)")
         
-        print("url : \(url)")
-        let placeholder = UIImage(named: "placeholder")
-        let processor = DownsamplingImageProcessor(size: iconImageView.bounds.size)
-        iconImageView.kf.indicatorType = .activity
-        iconImageView.kf.setImage(
-            with: url,
-            placeholder: placeholder,
-            options: [
-                .processor(processor),
-                .scaleFactor(UIScreen.main.scale),
-                .transition(.fade(0.2)),
-                .cacheOriginalImage
-            ]
-        )
+        if url.pathExtension.lowercased() == "svg" {
+            // ใช้ SDWebImage โหลด SVG
+            iconImageView.sd_setImage(with: url, placeholderImage: UIImage(named: "ic_app"))
+        } else {
+            // ใช้ Kingfisher โหลด PNG/JPG
+            let processor = DownsamplingImageProcessor(size: iconImageView.bounds.size)
+            let placeholder = UIImage(named: "placeholder")
+            iconImageView.kf.indicatorType = .activity
+            iconImageView.kf.setImage(
+                with: url,
+                placeholder: placeholder,
+                options: [
+                    .processor(processor),
+                    .scaleFactor(UIScreen.main.scale),
+                    .transition(.fade(0.2)),
+                    .cacheOriginalImage
+                ]
+            )
+        }
     }
 }
+
